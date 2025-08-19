8589908be9f525325824b796751b8cf2daa0d45f
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-08-19 16:32:07 +0800

    feat: 初始化 Spring Boot 项目- 添加 Spring Boot 项目结构和基本配置 - 创建应用入口类和测试类 - 配置 Maven 构建工具 - 添加项目忽略文件和属性文件 - 设置 Maven Wrapper 脚本和配置

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+//    hello world
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
