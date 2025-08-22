3236a2ccdcfe7be7bf8f5ca224964dcdedbc8073
BigDragonlin <51269337+BigDragonlin@users.noreply.github.com>
2025-08-22 17:44:02 +0800

    refactor(demo): 重构示例应用程序
    - configuration
    - 移除了 application.properties 文件
    - 更新了 DemoApplication.java，移除了注释代码- 修改了 StripePaymentService.java，添加了配置属性注入

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,21 +11,7 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-//        var orderService = context.getBean(OrderService.class);
-
-//        OrderService os = new OrderService();
-//        os.setPaymentService(new StripePaymentService());
-//        os.setPaymentService(new PayPalPaymentService());
-//        orderService.createOrder(100);
-
-//        String[] beans = context.getBeanDefinitionNames();
-//        for (String bean : beans) {
-//            System.out.println(bean);
-//        }
-
-        var sendMessageManagement = context.getBean(SendMessageMangement.class);
-        sendMessageManagement.sendMessage("Hello, world!", "EMAIL");
-        sendMessageManagement.sendMessage("Hello, world!", "SMS");
+        var orderService = context.getBean(OrderService.class);
+        orderService.createOrder(100);
     }
-
 }
