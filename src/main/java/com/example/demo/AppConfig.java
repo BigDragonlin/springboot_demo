4a6e0fa4014d2fd580f0f51519a2c413e9be3c8b
bghpc <1363992060@qq.com>
2025-08-23 14:32:49 +0800

    Bean Lifecycle

--- src/main/java/com/example/demo/AppConfig.java
+++ src/main/java/com/example/demo/AppConfig.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,18 @@ public class AppConfig {
 
     @Value("${payment.type:stripe}")
     private String paymentType;
+
+    //在bean初始化后执行
+    @PostConstruct
+    public void init(){
+        System.out.println("print init");
+    }
+
+    //在bean销毁后执行
+    @PreDestroy
+    public void destroy(){
+        System.out.println("print destroy");
+    }
 
     @Bean
     public OrderService orderService() {
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,15 +4,17 @@ import com.example.demo.SendMssageService.SendMessageMangement;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-        var heavyResource = context.getBean(HeavyResource.class);
-        var heavyResource1 = context.getBean(HeavyResource.class);
+//        ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+//        var sendMessageMangement = context.getBean(SendMessageMangement.class);
 
+         ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
+         context.close();
     }
 }
